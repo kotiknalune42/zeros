3758a0d1a0a85c7ed03db74d60b7a2a79a11f2f4
Катрин Райлян <k.shalamowa@gmail.com>
2018-09-30 20:13:03 +0300

    updetaed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 module.exports = function getZerosCount(number) {
-  
-  var zeros = 0;
-  
-  // A trailing zero is formed when a 
-  // multiple of 5 is multiplied with a multiple of 2.
-  
+  let zeros = 0; // A trailing zero is formed when a multiple of 5 is multiplied with a multiple of 2.
   while ( number > 0) {
-    number = Math.floor(number / 5);
+    number = Math.floor(number / 5); // round to nearest whole int
     zeros += number
   }
   return zeros;
